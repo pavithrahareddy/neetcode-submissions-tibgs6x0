33750146e55941003e39733254240835b4700806
Pavithra H A <62985721+pavithrahareddy@users.noreply.github.com>
2026-04-08 19:16:19 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        i,j = 0,len(s)-1
+        while i<j:
+            if s[i]!=t[j]:
+                return False
+            i = i+1
+            j =j-1
+        return True

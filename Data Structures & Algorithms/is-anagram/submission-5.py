62527233eca6458125348e15f7cb175ebc20e7f8
Pavithra H A <62985721+pavithrahareddy@users.noreply.github.com>
2026-05-07 17:55:21 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sc = {}
+        tc = {}
+        if len(s)!=len(t):
+            return False
+        for i in range(0,len(s)):
+            sc[s[i]] = 1 + sc.get(s[i],0)
+            tc[t[i]] = 1 + sc.get(t[i],0)
+        if sc!=tc:
+            return False
+        return true
+        
